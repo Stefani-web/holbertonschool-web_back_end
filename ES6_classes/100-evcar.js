0feecdbcef03d011a847bc708f4f41205c1ebe56
Stefani-web <stephaniecvlho@gmail.com>
2024-07-17 19:11:08 +0200

    Import Car from 10-car.js.
    Implement a class named EVCar that extends the Car class

---
+++ ES6_classes/100-evcar.js
@@ -0,0 +1,21 @@
+import Car from './10-car.js';
+
+class EVCar extends Car {
+  constructor(brand, motor, color, range) {
+    super();
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
+    this._range = range;
+  }
+
+  cloneCar() {
+    const clonedCar = new Car();
+    clonedCar._brand = this._brand;
+    clonedCar._motor = this._motor;
+    clonedCar._color = this._color;
+    return clonedCar;
+  }
+}
+
+export default EVCar;
