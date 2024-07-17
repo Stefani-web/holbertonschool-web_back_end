6fad2ecedd4d3fc7fcc98705ab8610599e1fb9e3
Stefani-web <stephaniecvlho@gmail.com>
2024-07-17 19:06:12 +0200

    Fix this code and make it work

--- ES6_classes/9-hoisting.js
+++ ES6_classes/9-hoisting.js
@@ -13,13 +13,14 @@ export class HolbertonClass {
   }
 }
 
-import { HolbertonClass } from "./9-hoisting.js"; // Adjust import path if necessary
+const class2019 = new HolbertonClass(2019, 'San Francisco');
+const class2020 = new HolbertonClass(2020, 'San Francisco');
 
 export class StudentHolberton {
   constructor(firstName, lastName, holbertonClass) {
     this._firstName = firstName;
     this._lastName = lastName;
-    this._holbertonClass = holbertonClass; // Fixed variable name
+    this._holbertonClass = holbertonClass;
   }
 
   get fullName() {
@@ -27,16 +28,13 @@ export class StudentHolberton {
   }
 
   get holbertonClass() {
-    return this._holbertonClass; // Fixed variable name
+    return this._holbertonClass;
   }
 
   get fullStudentDescription() {
-    return `${this.fullName} - ${this._holbertonClass.year} - ${this._holbertonClass.location}`;
+    return `${this._firstName} ${this._lastName} - ${this._holbertonClass.year} - ${this._holbertonClass.location}`;
   }
 }
-
-const class2019 = new HolbertonClass(2019, 'San Francisco');
-const class2020 = new HolbertonClass(2020, 'San Francisco');
 
 const student1 = new StudentHolberton('Guillaume', 'Salva', class2020);
 const student2 = new StudentHolberton('John', 'Doe', class2020);
@@ -44,4 +42,6 @@ const student3 = new StudentHolberton('Albert', 'Clinton', class2019);
 const student4 = new StudentHolberton('Donald', 'Bush', class2019);
 const student5 = new StudentHolberton('Jason', 'Sandler', class2019);
 
-export const listOfStudents = [student1, student2, student3, student4, student5];
+const listOfStudents = [student1, student2, student3, student4, student5];
+
+export default listOfStudents;
