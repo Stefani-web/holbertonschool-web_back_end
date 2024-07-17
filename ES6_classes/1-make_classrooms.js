9f707174c580848b67bd859e6f67ea599174a87b
Stefani-web <stephaniecvlho@gmail.com>
2024-07-17 18:07:04 +0200

    Import the ClassRoom class from 0-classroom.js

--- ES6_classes/1-make_classrooms.js
+++ ES6_classes/1-make_classrooms.js
@@ -1,9 +1,11 @@
 import ClassRoom from './0-classroom.js';
 
-export default function initializeRooms() {
+function initializeRooms() {
   return [
     new ClassRoom(19),
     new ClassRoom(20),
     new ClassRoom(34)
   ];
 }
+
+export default initializeRooms;
