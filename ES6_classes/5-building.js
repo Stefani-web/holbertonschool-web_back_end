04b0bfbfbafdc1d526280c0238cf0363d08ac208
Stefani-web <stephaniecvlho@gmail.com>
2024-07-17 18:29:15 +0200

    Fix Implement a class named Building

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,17 +1,19 @@
 class Building {
   constructor(sqft) {
-    if (new.target === Building) {
-      throw new TypeError('Cannot construct Building instances directly');
+    if (
+      this.constructor !== Building
+      && typeof this.evacuationWarningMessage !== 'function'
+    ) {
+      throw Error(
+        'Class extending Building must override evacuationWarningMessage',
+      );
     }
+
     this._sqft = sqft;
   }
 
   get sqft() {
     return this._sqft;
-  }
-
-  evacuationWarningMessage() {
-    throw new Error('Class extending Building must override evacuationWarningMessage');
   }
 }
 
