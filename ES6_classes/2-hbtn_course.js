21bacfd3ac9f1dae9b5c2b21e62cb686fad75d3a
Stefani-web <stephaniecvlho@gmail.com>
2024-07-17 18:09:02 +0200

    Implement a class named HolbertonCourse

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -1,55 +1,51 @@
-export default class HolbertonCourse {
+class HolbertonCourse {
   constructor(name, length, students) {
-    this._name = this._validateString(name, 'Name');
-    this._length = this._validateNumber(length, 'Length');
-    this._students = this._validateArrayOfStrings(students, 'Students');
+    this.name = name;
+    this.length = length;
+    this.students = students;
   }
 
-  // Getters
+  // name
   get name() {
     return this._name;
   }
 
+  set name(name) {
+    if (typeof name === 'string') {
+      this._name = name;
+    } else {
+      throw new TypeError('Name must be a string');
+    }
+  }
+
+  // length
   get length() {
     return this._length;
   }
 
+  set length(length) {
+    if (typeof length === 'number') {
+      this._length = length;
+    } else {
+      throw new TypeError('Length must be a number');
+    }
+  }
+
+  // students
   get students() {
     return this._students;
   }
 
-  // Setters
-  set name(newName) {
-    this._name = this._validateString(newName, 'Name');
-  }
-
-  set length(newLength) {
-    this._length = this._validateNumber(newLength, 'Length');
-  }
-
-  set students(newStudents) {
-    this._students = this._validateArrayOfStrings(newStudents, 'Students');
-  }
-
-  // Validation methods
-  _validateString(value, attributeName) {
-    if (typeof value !== 'string') {
-      throw new TypeError(`${attributeName} must be a string`);
+  set students(students) {
+    if (
+      Array.isArray(students)
+      && students.every((s) => typeof s === 'string')
+    ) {
+      this._students = students;
+    } else {
+      throw new TypeError('Students must be an array of strings');
     }
-    return value;
-  }
-
-  _validateNumber(value, attributeName) {
-    if (typeof value !== 'number') {
-      throw new TypeError(`${attributeName} must be a number`);
-    }
-    return value;
-  }
-
-  _validateArrayOfStrings(value, attributeName) {
-    if (!Array.isArray(value) || !value.every(item => typeof item === 'string')) {
-      throw new TypeError(`${attributeName} must be an array of strings`);
-    }
-    return value;
   }
 }
+
+export default HolbertonCourse;
