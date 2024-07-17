6764d1050d169bf4fbc5a99fbffc6c24df10d5d3
Stefani-web <stephaniecvlho@gmail.com>
2024-07-17 19:07:45 +0200

    Implement a class named Car

--- ES6_classes/10-car.js
+++ ES6_classes/10-car.js
@@ -1,31 +1,17 @@
-// 10-car.js
-
-const _brand = Symbol('brand');
-const _motor = Symbol('motor');
-const _color = Symbol('color');
-
 class Car {
   constructor(brand, motor, color) {
-    this[_brand] = brand;
-    this[_motor] = motor;
-    this[_color] = color;
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
   }
 
-  get brand() {
-    return this[_brand];
-  }
-
-  get motor() {
-    return this[_motor];
-  }
-
-  get color() {
-    return this[_color];
+  static get [Symbol.species]() {
+    return this;
   }
 
   cloneCar() {
-    // Create a new instance of the current class and pass attribute values
-    return new this.constructor(this[_brand], this[_motor], this[_color]);
+    const Species = this.constructor[Symbol.species];
+    return new Species();
   }
 }
 
