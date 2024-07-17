c9e8fb7932495e29e5a5f2e30213dbbfcc039a8c
Stefani-web <stephaniecvlho@gmail.com>
2024-07-17 18:40:14 +0200

    Fix Implement a class named Airport

--- ES6_classes/7-airport.js
+++ ES6_classes/7-airport.js
@@ -1,10 +1,12 @@
-export default class Airport {
+class Airport {
   constructor(name, code) {
-      this._name = name;
-      this._code = code;
+    this._name = name;
+    this._code = code;
   }
 
-  toString() {
-      return `[object ${this._code}]`;
+  get [Symbol.toStringTag]() {
+    return this._code;
   }
 }
+
+export default Airport;
