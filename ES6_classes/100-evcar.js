3a97b6844c5541061a5b8e8f88117f976240de26
Stefani-web <stephaniecvlho@gmail.com>
2024-07-17 19:14:20 +0200

    Fix Implement a class named EVCar that extends the Car class bis

--- ES6_classes/100-evcar.js
+++ ES6_classes/100-evcar.js
@@ -18,8 +18,4 @@ class EVCar extends Car {
   }
 }
 
-const ec1 = new EVCar("Tesla", "Turbo", "Red", "250");
-console.log(ec1);
-
-const ec2 = ec1.cloneCar();
-console.log(ec2);
+export default EVCar;
