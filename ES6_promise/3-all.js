2544747ad159a5f19fa3e6729f8f71e98e381dd6
Stefani-web <stephaniecvlho@gmail.com>
2024-07-23 12:19:19 +0200

    Fix and debug

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -1,12 +1,12 @@
 import { uploadPhoto, createUser } from './utils.js';
 
 export default function handleProfileSignup() {
-    return Promise.all([uploadPhoto(), createUser()])
-        .then((results) => {
-            const [photoResponse, userResponse] = results;
-            console.log(`${photoResponse.body} ${userResponse.firstName} ${userResponse.lastName}`);
-        })
-        .catch(() => {
-            console.log('Signup system offline');
-        });
+  return Promise.all([uploadPhoto(), createUser()])
+    .then((results) => {
+      const [photoResponse, userResponse] = results;
+      console.log(`${photoResponse.body} ${userResponse.firstName} ${userResponse.lastName}`);
+    })
+    .catch(() => {
+      console.log('Signup system offline');
+    });
 }
