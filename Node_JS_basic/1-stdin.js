0ede2ef0e69f6d53c35f5dee05c400aec65702e2
Stefani-web <stephaniecvlho@gmail.com>
2024-09-16 13:17:21 +0200

    Fix my errors - Debbug

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,18 +1,18 @@
 // Displays a welcome message
-console.log("Welcome to Holberton School, what is your name?");
+console.log('Welcome to Holberton School, what is your name?');
 
 // Capture user input via STDIN
 process.stdin.on('data', (input) => {
   const name = input.toString().trim(); // Removes leading and trailing whitespace
   console.log(`Your name is: ${name}`);
 
-// If the input comes from a pipe (as with "echo"), close the program properly
-if (process.stdin.isTTY) {
-  // Interactive mode: no closing message, the program waits for the next entry
-  process.stdin.end();
-} else {
-  // Non-interactive mode: close message and exit
-  console.log("This important software is now closing");
-  process.exit();
-}
+  // If the input comes from a pipe (as with "echo"), close the program properly
+  if (process.stdin.isTTY) {
+    // Interactive mode: no closing message, the program waits for the next entry
+    process.stdin.end();
+  } else {
+    // Non-interactive mode: close message and exit
+    console.log('This important software is now closing');
+    process.exit();
+  }
 });
